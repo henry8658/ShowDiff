28ae5c16571e81d479db93f83d14091e3ea9adae
Henry <henry8658@gmail.com>
2017-07-08 23:43:17 -0700

    Initial commit

---
+++ regex.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class regex {
+    public static void main(String [] args) {
+
+        String fileName1= "C:/Users/Henry/Desktop/Hello java.txt";
+    try {
+        FileReader fileReader =
+             new FileReader(fileName1);
+        String line = null;
+         // Always wrap FileReader in BufferedReader.
+         BufferedReader objreader =
+             new BufferedReader(fileReader);
+
+        while ((line = objreader.readLine()) != null) {
+            System.out.println(line);
+        }
+        objreader.close();
+
+    }
+    catch(FileNotFoundException ex) {
+        System.out.println("FilePath Not Found");
+    }
+
+    catch(IOException ex) {
+        System.out.println("Error reading file");
+    }
+    }
+
+}
