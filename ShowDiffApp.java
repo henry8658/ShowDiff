7faf5ab04f3f48071989820b4a6eff8fa937de63
Henry <hanry.park3@gmail.com>
2017-07-09 23:48:47 -0700

    handle leftover and encapsulation

--- ShowDiffApp.java
+++ ShowDiffApp.java
@@ -2,6 +2,10 @@ import java.io.*;
 
 public class ShowDiffApp {
     private static final String OUTPUT = "output.txt";
+    private static final String BOUNDARY = "==========\n";
+    private static final String BOUNDARY2 = "----------\n";
+    private static final String FILE1 = "FILE1:\n";
+    private static final String FILE2 = "FILE2:\n";
 
     public static void main(String [] args) {
         ShowDiffApp app = new ShowDiffApp();
@@ -11,41 +15,6 @@ public class ShowDiffApp {
     }
 
     public ShowDiffApp() {
-
-    }
-
-    //TODO: takes in 2 lines, return them in the format of
-    // ===========
-    // FILE1:
-    // something
-    // -----------
-    // FILE2:
-    // stuff that is different
-    // ===========
-    public String differentLine(String line1, String line2) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("==========\n");
-        sb.append("FILE1:\n");
-        sb.append(line1 + "\n");
-        sb.append("----------\n");
-        sb.append("FILE2:\n");
-        sb.append(line2 + "\n");
-        sb.append("==========\n");
-
-        return sb.toString();
-    }
-
-    public String sameLine(String line) {
-        return line + "\n";
-    }
-
-    public boolean same(String input1, String input2) {
-        boolean same = false;
-
-        if (input1.equals(input2)) {
-            same = true;
-        }
-        return same;
     }
 
     public void run(String input1, String input2, String output) {
@@ -53,18 +22,8 @@ public class ShowDiffApp {
         BufferedReader bufferedReader2 = readfile(input2);
         BufferedWriter bufferedWriter = writeFile(output);
 
-        String line2, line1 = null;
-
         try {
-            while ((line1 = bufferedReader1.readLine()) != null
-                    && (line2 = bufferedReader2.readLine()) != null) {
-                //System.out.println(line);
-                if (same(line1, line2)) {
-                    bufferedWriter.write(sameLine(line1));
-                } else {
-                    bufferedWriter.write(differentLine(line1, line2));
-                }
-            }
+            handleComparison(bufferedWriter, bufferedReader1, bufferedReader2);
 
             bufferedReader1.close();
             bufferedReader2.close();
@@ -75,7 +34,66 @@ public class ShowDiffApp {
         }
     }
 
-    public BufferedReader readfile(String input) {
+    private String leftOverLine(int file, String line, BufferedReader bufferedReader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BOUNDARY);
+        sb.append("FILE" + file + " LEFT:\n");
+        sb.append(line + "\n");
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        return sb.toString();
+    }
+
+    private void handleLeftOver(String line1, String line2, BufferedWriter bufferedWriter, BufferedReader bufferedReader1, BufferedReader bufferedReader2) throws IOException {
+        if (line1 != null) {
+            bufferedWriter.write(leftOverLine(1, line1, bufferedReader1));
+        } else if (line2 != null) {
+            bufferedWriter.write(leftOverLine(2, line2, bufferedReader2));
+        }
+    }
+
+    private void handleComparison(BufferedWriter bufferedWriter, BufferedReader bufferedReader1, BufferedReader bufferedReader2) throws IOException {
+        String line1 = null, line2 = null;
+        while ((line1 = bufferedReader1.readLine()) != null
+                && (line2 = bufferedReader2.readLine()) != null) {
+            if (same(line1, line2)) {
+                bufferedWriter.write(sameLine(line1));
+            } else {
+                bufferedWriter.write(differentLine(line1, line2));
+            }
+        }
+
+        handleLeftOver(line1, line2, bufferedWriter, bufferedReader1, bufferedReader2);
+    }
+
+    private String differentLine(String line1, String line2) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BOUNDARY);
+        sb.append(FILE1);
+        sb.append(line1 + "\n");
+        sb.append(BOUNDARY2);
+        sb.append(FILE2);
+        sb.append(line2 + "\n");
+        sb.append(BOUNDARY);
+
+        return sb.toString();
+    }
+
+    private String sameLine(String line) {
+        return line + "\n";
+    }
+
+    private boolean same(String input1, String input2) {
+        boolean same = false;
+
+        if (input1.equals(input2)) {
+            same = true;
+        }
+        return same;
+    }
+
+    private BufferedReader readfile(String input) {
         try {
             FileReader fileReader =
                  new FileReader(input);
@@ -90,7 +108,7 @@ public class ShowDiffApp {
         return null;
     }
 
-    public BufferedWriter writeFile(String output) {
+    private BufferedWriter writeFile(String output) {
         try {
             FileWriter writer = new FileWriter(output, true);
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
